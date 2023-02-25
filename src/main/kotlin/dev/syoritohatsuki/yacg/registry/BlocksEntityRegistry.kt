78e6d10b195e279a8b2f1e273f2fc6a952124444
Galiev <ggvalion@gmail.com>
2023-02-25 19:39:42 +0500

    Added blocks generation to the block inventory and fixed the bug with exit Minecraft

---
+++ src/main/kotlin/dev/syoritohatsuki/yacg/registry/BlocksEntityRegistry.kt
@@ -0,0 +1,19 @@
+package dev.syoritohatsuki.yacg.registry
+
+import dev.syoritohatsuki.yacg.YetAnotherCobbleGen
+import dev.syoritohatsuki.yacg.common.block.entity.GeneratorBlockEntity
+import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder
+import net.minecraft.block.entity.BlockEntityType
+import net.minecraft.registry.Registries
+import net.minecraft.registry.Registry
+import net.minecraft.util.Identifier
+
+object BlocksEntityRegistry {
+    val GENERATOR_ENTITY: BlockEntityType<GeneratorBlockEntity> =
+        Registry.register(
+            Registries.BLOCK_ENTITY_TYPE,
+            Identifier(YetAnotherCobbleGen.MOD_ID, "generator_block_entity"),
+            FabricBlockEntityTypeBuilder.create(::GeneratorBlockEntity, BlocksRegistry.COBBLE_GENERATOR
+            ).build()
+        )
+}
