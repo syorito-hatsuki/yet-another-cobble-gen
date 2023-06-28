7835513b01b4fb3205db21e4c970ea39e6e3431e
syorito-hatsuki <enderdron3@gmail.com>
2023-06-28 03:45:12 +0300

    Removed files

--- src/main/kotlin/dev/syoritohatsuki/yacg/integration/rei/TableDisplay.kt
+++
@@ -1,9 +0,0 @@
-package dev.syoritohatsuki.yacg.integration.rei
-
-import dev.syoritohatsuki.yacg.integration.rei.YacgReiPlugin.GENERATORS_CATEGORY
-import me.shedaniel.rei.api.common.display.basic.BasicDisplay
-import me.shedaniel.rei.api.common.entry.EntryIngredient
-
-class TableDisplay(input: EntryIngredient, outputs: List<EntryIngredient>) : BasicDisplay(listOf(input), outputs) {
-    override fun getCategoryIdentifier() = GENERATORS_CATEGORY
-}
